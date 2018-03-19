80feb9d0ee5551068cfe583effddae7dadc332b9
pablootesaez <oterosaezp@gmail.com>
2018-03-19 12:24:07 +0100

    borrado proyecto anterior

--- starter-code/public/javascript/editProduct.js
+++
@@ -1,9 +0,0 @@
-//var inputs = document.getElementsByClassName('editButton');
-//var inputs = document.getElementsByClassName('hidden');
-$(document).ready(function(){
-  $(".editButton").click(function(){
-    console.log("hola");
-    $(this).parent().siblings().toggleClass("d-none");
-    $(this).parent().toggleClass("d-none");
-  });
-});
